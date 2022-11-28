08964bf345b723feda7a23cd4e97627744b7cb9f
vadim <vadim.presman@gmail.com>
2022-11-28 23:06:40 +0300

    codewars_reflection_3

---
+++ TMS_Eigth_App/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Runtime.Serialization.Json;
+using System.Security.Principal;
+using System.Text;
+
+namespace TMS_Eigth_App
+{
+    internal class Program
+    {
+        public void Main(string[] args)
+        {
+            var type = typeof(Car);
+
+            var methods = type.GetMethods();
+
+            foreach (var method in methods)
+            {
+                Console.WriteLine(method);
+            }
+        }
+    }
+
+    class Car
+    {
+        private void Speed()
+        {
+            
+        }
+        protected void KmMove()
+        {
+            
+        }
+        public void CarName()
+        {
+
+        } 
+
+        public Car()
+        {
+
+        }
+    }
+}
